37419e06d80e15c9d47ac01473a6c88273d9a300
Mikhail Zharkov <Mikhail Zharkov>
2016-12-05 20:55:26 +0300

    fileUpload

---
+++ backend/ApiServer/Controllers/UploadController.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+namespace ApiServer.Controllers
+{
+    public class UploadController : ApiController
+    {
+        // POST: api/Upload
+        public HttpResponseMessage Post()
+        {
+            var file = HttpContext.Current.Request.Files[0];
+
+            if (file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(file.FileName);
+                var path = Path.Combine(HttpContext.Current.Server.MapPath("~/App_Data/uploads"), fileName);
+                file.SaveAs(path);
+                var content = JsonConvert.SerializeObject(fileName, new JsonSerializerSettings
+                {
+                    ContractResolver = new CamelCasePropertyNamesContractResolver()
+                });
+
+                var response = Request.CreateResponse(HttpStatusCode.OK);
+                response.Content = new StringContent(content, Encoding.UTF8, "application/json");
+                return response;
+            }
+            else
+            {
+                var response = Request.CreateResponse(HttpStatusCode.InternalServerError);
+                response.Content = new StringContent("error!!!", Encoding.UTF8, "application/json");
+                return response;
+            }
+        }
+    }
+}
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,19 +1,22 @@
 import { Component } from '@angular/core';
+import { UploadService } from './shared/upload.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
+  providers: [ UploadService ]
 })
 export class AppComponent {
   title = 'PC Config!';
   navList: INavItem[] = [];
   auth: INavItem;
   myColor = "primary";
-  isAuth:boolean;
   //"primary" | "accent" | "warn"
+  isAuth:boolean;
+  picName: any;
 
-  constructor() {
+  constructor(private service: UploadService) {
     this.navList = [
       { address: "/configurator", text: "Сборка" },
       { address: "/examples", text: "Шаблоны" },
@@ -21,8 +24,28 @@ export class AppComponent {
     ]
     this.auth = { address: "/auth", text: "Авторизация" };
     this.isAuth = false;
-    
+
+    this.service.progress$.subscribe(
+      (data) => {
+        console.log('progress = ' + data);
+      }
+    );
   }
+  
+
+
+  onChange(event) {
+    console.log('onChange', event);
+    var files = event.srcElement.files;
+    console.log(files);
+    this.service.makeFileRequest('http://localhost:27835/api/upload', [], files).subscribe(
+      (fileName) => {
+        console.log('sent');
+        this.picName = fileName;
+      }
+    );
+  }  
+
 }
 
 interface INavItem {
